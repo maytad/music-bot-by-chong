28b62b498d2d731cd066ee25c0f0716d1b3373f2
aoey <metest.t@fillgoods.co>
2021-10-01 16:33:04 +0700

    init project

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  app: {
+    px: "!",
+    token: "ODkzNDAzNDM3NTUzODE5Njg5.YVa8yg.omk6cOjUx9u9vuABQk0qIT70T-U",
+    playing: "Chong Za",
+  },
+
+  opt: {
+    DJ: {
+      enabled: true,
+      roleName: "DJ",
+      commands: [
+        "back",
+        "clear",
+        "filter",
+        "loop",
+        "pause",
+        "resume",
+        "seek",
+        "shuffle",
+        "skip",
+        "stop",
+        "volume",
+      ],
+    },
+    maxVol: 100,
+    discordPlayer: {},
+  },
+};
